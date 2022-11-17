25b84b93987d3944b4692223c8918e573bea91f1
Manav Misra <manavm@visionwebsoft.com>
2022-11-16 20:02:22 -0600

    Render list of users from API service

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+import apiService from "./services/api.service";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    apiService.getAllUsers().then((users) => setUsers(users));
+  });
+
+  return (
+    <>
+      <h1 className="heading">Hello World</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
