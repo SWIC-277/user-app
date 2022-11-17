054a28c835864d83e949892d88d21bf7cb541572
Manav Misra <manavm@visionwebsoft.com>
2022-11-16 20:40:39 -0600

    Log username and password from form

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import { useEffect, useState } from "react";
-import apiService from "./services/api.service";
+import Login from "./components/Login";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    apiService.getAllUsers().then((users) => setUsers(users));
-  });
-
   return (
-    <>
-      <h1 className="heading">Hello World</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </>
+    <main className="flex justify-center pt-8">
+      <Login />
+    </main>
   );
 }
 
